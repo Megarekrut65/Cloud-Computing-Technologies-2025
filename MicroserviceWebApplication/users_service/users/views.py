df4369cc8f9fcfef768e3e63048f413335096f07
Megarekrut65 <bandalak28@gmail.com>
2025-03-02 13:02:25 +0200

    Created service for users registration and authitication

---
+++ MicroserviceWebApplication/users_service/users/views.py
@@ -0,0 +1,25 @@
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer, RegisterSerializer, LoginSerializer
+
+User = get_user_model()
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.validated_data)
